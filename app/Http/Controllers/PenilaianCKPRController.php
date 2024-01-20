4ad7057fff79d4e5f473788414ccd198276b9bac
wisnusetianto <chrmgawain@gmail.com>
2024-01-20 14:55:59 +0700

    update

--- app/Http/Controllers/PenilaianCKPRController.php
+++ app/Http/Controllers/PenilaianCKPRController.php
@@ -10,32 +10,32 @@ use Illuminate\Http\Request;
 class PenilaianCKPRController extends Controller
 {
     //Read
-    public function kepalabps_index(Request $request)
+    public function index(Request $request)
     {
         $nilaickpr = penilaian_ckpr::all();
         return view('pages.users.kepalabps.penilaianckpr.index', compact(['nilaickpr']));
     }
 
     //Create
-    public function kepalabps_create(Request $request)
+    public function create(Request $request)
     {
         return view('pages.users.kepalabps.penilaianckpr.create');
     }
 
-    public function kepalabps_store(Request $request)
+    public function store(Request $request)
     {
         penilaian_ckpr::create($request->except(['_token','submit']));
         return redirect('/kepalabps-ckp/penilaianckpr');
     }
 
    //Update
-   public function kepalabps_edit($id)
+   public function edit($id)
    {
        $nilaickpr = penilaian_ckpr::find($id);
        return view('pages.users.kepalabps.penilaianckpr.edit', compact(['nilaickpr']));
    }
 
-   public function kepalabps_update($id, Request $request)
+   public function update($id, Request $request)
    {
        $nilaickpr = penilaian_ckpr::find($id);
        $nilaickpr->update($request->except(['_token','submit']));
@@ -43,7 +43,7 @@ class PenilaianCKPRController extends Controller
    }
 
    //Destroy
-   public function kepalabps_destroy($id)
+   public function destroy($id)
    {
        $nilaickpr = penilaian_ckpr::find($id);
        $nilaickpr->delete();
