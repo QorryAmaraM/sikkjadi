dcbb4eb0cba0c689a3b3b0a8bb8de6c5b05c1eb7
QorryAmaraM <qorryamaram1@gmail.com>
2024-01-21 13:17:40 +0700

    edit create nilai skp

--- resources/views/pages/users/staf/penilaianskp/create.blade.php
+++ resources/views/pages/users/staf/penilaianskp/create.blade.php
@@ -71,46 +71,6 @@
             <div class="row mb-8">
                 <div class="col-sm-12">
                     <div class="form-group">
-                        <label for="jenisrk">Jenis</label>
-                        <input type="jenisrk" class="form-control" id="jenisrk" placeholder="Lorem Ipsum Dolor Sit Amet"
-                            name="jenis">
-                    </div>
-                    <div class="form-group">
-                        <label for="rkatasan">Rencana Kinerja Atasan</label>
-                        <input type="rkatasan" class="form-control" id="rkatasan" placeholder="Lorem Ipsum Dolor Sit Amet"
-                            name="rencana_kinerja_atasan">
-                    </div>
-                    <div class="form-group">
-                        <label for="rkpegawai">Rencana Kinerja</label>
-                        <input type="rkpegawai" class="form-control" id="rkpegawai"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="rencana_kinerja">
-                    </div>
-                    <div class="form-group">
-                        <label for="aspek">Aspek</label>
-                        <input type="aspek" class="form-control" id="aspek"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="aspek">
-                    </div>
-                    <div class="form-group">
-                        <label for="iki">IKI</label>
-                        <input type="iki" class="form-control" id="iki"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="iki">
-                    </div>
-                    <div class="form-group">
-                        <label for="targetmin">Target Min</label>
-                        <input type="targetmin" class="form-control" id="targetmin"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="target_min">
-                    </div>
-                    <div class="form-group">
-                        <label for="targetmix">Target Max</label>
-                        <input type="targetmix" class="form-control" id="targetmix"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="target_max">
-                    </div>
-                    <div class="form-group">
-                        <label for="satuan">Satuan</label>
-                        <input type="satuan" class="form-control" id="satuan"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="satuan">
-                    </div>
-                    <div class="form-group">
                         <label for="realisasi">Realisasi</label>
                         <input type="realisasi" class="form-control" id="realisasi"
                             placeholder="Lorem Ipsum Dolor Sit Amet" name="realisasi">
@@ -119,46 +79,6 @@
                         <label for="kondisi">Kondisi</label>
                         <input type="kondisi" class="form-control" id="kondisi"
                             placeholder="Lorem Ipsum Dolor Sit Amet" name="kondisi">
-                    </div>
-                    <div class="form-group">
-                        <label for="capaianiki">Capaian IKI</label>
-                        <input type="capaianiki" class="form-control" id="capaianiki"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="capaian_iki">
-                    </div>
-                    <div class="form-group">
-                        <label for="kategoriki">Kategori Capaian IKI</label>
-                        <input type="kategoriki" class="form-control" id="kategoriki"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="kategori_capaian_iki">
-                    </div>
-                    <div class="form-group">
-                        <label for="nilaicapaian">Nilai Capaian Rencana</label>
-                        <input type="nilaicapaian" class="form-control" id="nilaicapaian"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="nilai_capaian_rencana">
-                    </div>
-                    <div class="form-group">
-                        <label for="kategoricp">Kategori Capaian Rencana</label>
-                        <input type="kategoricp" class="form-control" id="kategoricp"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="kategori_capaian_rencana">
-                    </div>
-                    <div class="form-group">
-                        <label for="nilaitimbang">Nilai Tertimbang</label>
-                        <input type="nilaitimbang" class="form-control" id="nilaitimbang"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="nilai_tertimbang">
-                    </div>
-                    <div class="form-group">
-                        <label for="nilaiutama">Nilai Kinerja Utama</label>
-                        <input type="nilaiutama" class="form-control" id="nilaiutama"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="nilai_kinerja_utama">
-                    </div>
-                    <div class="form-group">
-                        <label for="nilaitambahan">Nilai Kinerja Tambahan</label>
-                        <input type="nilaitambahan" class="form-control" id="nilaitambahan"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="nilai_kinerja_tambahan">
-                    </div>
-                    <div class="form-group">
-                        <label for="nilaiskp">Nilai SKP</label>
-                        <input type="nilaiskp" class="form-control" id="nilaiskp"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="nilai_skp">
                     </div>
                 </div>
             </div>
