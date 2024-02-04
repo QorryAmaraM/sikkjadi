2984176840eb7b1907d9f4b9523be0b4b42f45ee
wisnusetianto <chrmgawain@gmail.com>
2024-02-04 22:55:36 +0700

    update

--- resources/views/pages/admin/penilaianskp/index.blade.php
+++ resources/views/pages/admin/penilaianskp/index.blade.php
@@ -93,21 +93,38 @@
                     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                         <thead>
                             <tr>
-                                
+                                <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Jenis</th>
+                                <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Rencana Kinerja Atasan </th>
+                                <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Rencana Kinerja</th>
+                                <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Aspek</th>
+                                <th rowspan="2" style="padding:0.2rem; vertical-align: middle">IKI</th>
+                                <th colspan="2" style="padding:0.2rem; border-bottom: none">Target</th>
+                                <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Satuan</th>                            
                                 <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Realisasi</th>
                                 <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Kondisi</th>
                                 <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Capaian IKI</th>
                                 <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Kategori Capaian IKI</th>
                                 <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Nilai Capaian Rencana</th>
-                                <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Kategori Capaian Rencana
-                                </th>
+                                <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Kategori Capaian Rencana</th>
                                 <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Nilai Tertimbang</th>
                                 <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Aksi</th>
                             </tr>
+                            <tr>
+                                <th style="border-top: none">Min</th>
+                                <th style="border-top: none">Max</th>
+                            </tr>
                         </thead>
                         <tbody class="alldata">
-                            @foreach ($penilaianskp as $skp)
+                            @foreach ($result as $skp)
                                 <tr>
+                                    <td>{{ $skp->kinerja }}</td>
+                                    <td>{{ $skp->rencana_kinerja_atasan }}</td>
+                                    <td>{{ $skp->rencana_kinerja }}</td>
+                                    <td>{{ $skp->aspek }}</td>
+                                    <td>{{ $skp->iki }}</td>
+                                    <td>{{ $skp->target_min }}</td>
+                                    <td>{{ $skp->target_max }}</td>
+                                    <td>{{ $skp->satuan }}</td>
                                     <td>{{ $skp->realisasi }}</td>
                                     <td>{{ $skp->kondisi }}</td>
                                     <td>{{ $skp->capaian_iki }}</td>
