1459a255a1bd88780dfa25008829ba43e7c0c6e6
wisnusetianto <chrmgawain@gmail.com>
2024-02-04 23:11:31 +0700

    update

--- resources/views/pages/admin/penilaianskp/index.blade.php
+++ resources/views/pages/admin/penilaianskp/index.blade.php
@@ -105,15 +105,22 @@
                                 <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Capaian IKI</th>
                                 <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Kategori Capaian IKI</th>
                                 <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Nilai Capaian Rencana</th>
-                                <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Kategori Capaian Rencana
-                                </th>
+                                <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Kategori Capaian Rencana</th>
                                 <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Nilai Tertimbang</th>
-                                <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Aksi</th> 
+                                <th rowspan="2" style="padding:0.2rem; vertical-align: middle">Aksi</th>
                             </tr>
                         </thead>
                         <tbody class="alldata">
                             @foreach ($penilaianskp as $skp)
                                 <tr>
+                                    <td>{{ $skp->kinerja }}</td>
+                                    <td>{{ $skp->rencana_kinerja_atasan }}</td>
+                                    <td>{{ $skp->rencana_kinerja }}</td>
+                                    <td>{{ $skp->aspek }}</td>
+                                    <td>{{ $skp->iki }}</td>
+                                    <td>{{ $skp->target_min }}</td>
+                                    <td>{{ $skp->target_max }}</td>
+                                    <td>{{ $skp->satuan }}</td>
                                     <td>{{ $skp->realisasi }}</td>
                                     <td>{{ $skp->kondisi }}</td>
                                     <td>{{ $skp->capaian_iki }}</td>
