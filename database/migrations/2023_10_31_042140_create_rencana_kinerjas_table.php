b7b68b2738150a4f0644a752281f125ba29fa31c
wisnusetianto <chrmgawain@gmail.com>
2024-02-13 14:09:16 +0700

    update

--- database/migrations/2023_10_31_042140_create_rencana_kinerjas_table.php
+++ database/migrations/2023_10_31_042140_create_rencana_kinerjas_table.php
@@ -16,15 +16,23 @@ return new class extends Migration
         Schema::create('rencana_kinerjas', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('skp_tahunan_id');
-            $table->enum('kinerja', ['utama','tambahan'])->nullable();
+            $table->enum('kinerja', ['utama', 'tambahan'])->nullable();
             $table->string('jenis')->nullable();
             $table->string('rencana_kinerja_atasan')->nullable();
             $table->string('rencana_kinerja')->nullable();
             $table->string('aspek')->nullable();
-            $table->string('iki')->nullable();
-            $table->string('target_min')->nullable();
-            $table->string('target_max')->nullable();
-            $table->string('satuan')->nullable();
+            $table->string('kuantitas_iki')->nullable();
+            $table->string('kuantitas_target_min')->nullable();
+            $table->string('kuantitas_target_max')->nullable();
+            $table->string('kuantitas_satuan')->nullable();
+            $table->string('kualitas_iki')->nullable();
+            $table->string('kualitas_target_min')->nullable();
+            $table->string('kualitas_target_max')->nullable();
+            $table->string('kualitas_satuan')->nullable();
+            $table->string('waktu_iki')->nullable();
+            $table->string('waktu_target_min')->nullable();
+            $table->string('waktu_target_max')->nullable();
+            $table->string('waktu_satuan')->nullable();
             $table->timestamps();
 
             $table->foreign('skp_tahunan_id')->references('id')->on('skp_tahunans')->onDelete('cascade');
