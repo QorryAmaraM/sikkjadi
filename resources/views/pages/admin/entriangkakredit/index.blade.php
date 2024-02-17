2ab349e8fa90abb003421ba0ec6eb3a549dabe88
QorryAmaraM <qorryamaram1@gmail.com>
2024-02-17 20:01:55 +0700

    tes

--- resources/views/pages/admin/entriangkakredit/index.blade.php
+++ resources/views/pages/admin/entriangkakredit/index.blade.php
@@ -10,7 +10,7 @@
               </div>
 
               <!-- Content Row -->
-              <form action="/admin-masterangkakredit/entriangkakredit/store" method="POST">
+              <form action="/admin-masterangkakredit/entriangkakredit/store" method="POST" id="myForm">
                   @csrf
                   <div class="row mb-8">
                       <div class="col-sm-12">
@@ -48,15 +48,14 @@
                           </div>
                           <div class="form-group d-flex align-items-center">
                               <label for="angkakredit" class="col-sm-2 pl-0 col-form-label">Angka Kredit</label>
-                              <input type="text" class="form-control col-sm-10" name="angka_kredit" placeholder="Masukkan Angka" onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))" required>
+                              <input type="text" class="form-control col-sm-10" name="angka_kredit" placeholder="Masukkan Angka" onkeypress="return(event.charCode != 8 && event.charCode == 0 || (event.charCode >= 48 && event.charCode <= 57) || event.charCode == 46 || event.charCode == 44)" required>
                           </div>
                       </div>
                   </div>
 
                   <div class="row">
                       <div class="col-sm-12 mt-3 text-right">
-                          <button type="submit" name="submit" value="Save" class="btn save-button" data-toggle="modal"
-                              data-target="#successModal">+ Tambah</button>
+                          <button type="submit" name="submit" value="Save" class="btn save-button" onclick="checkFormAndShowModal()">+ Tambah</button>
                       </div>
                   </div>
           </div>
@@ -90,6 +89,30 @@
             }, 3000));
         });
     });
+
+    function checkFormAndShowModal() {
+        // Pemeriksaan apakah semua input telah diisi
+        var form = document.getElementById('myForm');
+        var allInputsFilled = true;
+
+        // Loop untuk memeriksa setiap input dalam form
+        for (var i = 0; i < form.length; i++) {
+            if (form[i].type == "text" || form[i].type == "select-one") {
+                if (form[i].value == "") {
+                    allInputsFilled = false;
+                    break;
+                }
+            }
+        }
+
+        // Jika semua input terisi, tampilkan modal
+        if (allInputsFilled) {
+            $('#successModal').modal('show');
+        } else {
+            alert("Harap isi semua data sebelum melanjutkan.");
+        }
+    }
+
 </script>
 
           <!-- /.container-fluid -->
