4c921e42360e39d822768041348d5700e5a35efe
QorryAmaraM <qorryamaram1@gmail.com>
2024-02-18 19:34:22 +0700

    tes

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -45,9 +45,9 @@
                                             <input type="hidden" name="token" value="{{ $token }}">
 
                                             <div class="row mb-3">
-                                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                                                <label for="email" class="col-md-4 label-fgpw text-md-end">{{ __('EMAIL ADDRESS') }}</label>
 
-                                                <div class="col-md-6">
+                                                <div class="col-md-7">
                                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
 
                                                     @error('email')
@@ -59,9 +59,9 @@
                                             </div>
 
                                             <div class="row mb-3">
-                                                <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                                                <label for="password" class="col-md-4 label-fgpw text-md-end">{{ __('PASSWORD') }}</label>
 
-                                                <div class="col-md-6">
+                                                <div class="col-md-7">
                                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                                     @error('password')
@@ -73,9 +73,9 @@
                                             </div>
 
                                             <div class="row mb-3">
-                                                <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+                                                <label for="password-confirm" class="col-md-4 label-fgpw text-md-end">{{ __('COMFIRM PASSWORD') }}</label>
 
-                                                <div class="col-md-6">
+                                                <div class="col-md-7">
                                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                                                 </div>
                                             </div>
