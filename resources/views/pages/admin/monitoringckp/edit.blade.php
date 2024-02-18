e9d297e15537058fb5ccd2dbbe0284846bc131ae
QorryAmaraM <qorryamaram1@gmail.com>
2024-02-18 21:23:26 +0700

    p

--- resources/views/pages/admin/monitoringckp/edit.blade.php
+++ resources/views/pages/admin/monitoringckp/edit.blade.php
@@ -52,7 +52,7 @@
                         <div class="col-sm-12">
                             <div class="form-group">
                                 <label for="kegiatan">CKPR Akhir</label>
-                                <input type="kegiatan" class="form-control" id="ckp_akhir" name="ckp_akhir">
+                                <input type="kegiatan" class="form-control" id="ckp_akhir" name="ckp_akhir" placeholder="Masukkan Angka" onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))" required>
                             </div>
                             <div class="form-group">
                                 <label for="kodebutir">Keterangan Kepala</label>
@@ -63,7 +63,7 @@
 
                     <div class="row">
                         <div class="col-sm-12 mt-3 text-right">
-                            <button type="submit" name="submit" value="Save" class="btn save-button">Simpan</button>
+                            <button type="submit" name="submit" value="Save" class="btn save-button" onclick="checkFormAndShowModal()">Simpan</button>
                         </div>
                     </div>
 
@@ -110,7 +110,7 @@
                         <div class="col-sm-12">
                             <div class="form-group">
                                 <label for="kegiatan">CKPR Akhir</label>
-                                <input type="text" class="form-control" id="ckp_akhir" name="ckp_akhir" value="{{ $result->ckp_akhir }}" required placeholder="Masukkan Angka" onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))" required>
+                                <input type="text" class="form-control" id="ckp_akhir" name="ckp_akhir" value="{{ $result->ckp_akhir }}"  placeholder="Masukkan Angka" onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))" required>
                             </div>
                             <div class="form-group">
                                 <label for="kodebutir">Keterangan Kepala</label>
