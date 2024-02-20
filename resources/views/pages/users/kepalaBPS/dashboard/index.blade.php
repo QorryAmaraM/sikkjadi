6f79ea576422c19b55e2dce1cdd2a7f0622bd8ed
wisnusetianto <chrmgawain@gmail.com>
2024-02-20 16:16:30 +0700

    update

--- resources/views/pages/users/kepalaBPS/dashboard/index.blade.php
+++ resources/views/pages/users/kepalaBPS/dashboard/index.blade.php
@@ -8,107 +8,113 @@
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             @foreach ($user as $users)
                 @if ($users->id == $userid)
-                <h1 class="h3 mb-0 text-black-800">Selamat Datang di Sistem Informasi Kinerja Karyawan BPS {{ $users->nama }}</h1>
+                    <h1 class="h3 mb-0 text-black-800">Selamat Datang di Sistem Informasi Kinerja Karyawan BPS {{ $users->nama }}</h1>
                 @endif
             @endforeach
             </h1>
         </div>
 
         <div class="container-fluid">
-    <div class="row">
-        <div class="col-sm-3 col-md-3 mx-auto">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Nilai SKP Tertinggi</h6>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body"> 
-                    {{ $nilai_skp_tertinggi }}                      
-                                     
-                </div>
-            </div>
-        </div>
+            <div class="row">
+                <div class="col-sm-3 col-md-3 mx-auto">
+                    <div class="card shadow mb-4">
+                        <a href="/kepalabps-perencanaankerja/penilaianskp">
+                            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                <h6 class="m-0 font-weight-bold text-primary">Nilai SKP Tertinggi</h6>
+                            </div>
+                            <!-- Card Body -->
+                            <div class="card-body">
+                                {{ $nilai_skp_tertinggi }}    
+                            </div>
 
-        <div class="col-sm-3 col-lg-3 mx-auto">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Nilai SKP Terendah</h6>
+                        </a>
+                        <!-- Card Header - Dropdown -->
+                    </div>
                 </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    {{ $nilai_skp_terendah }}                    
-                                     
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <div class="row">
-        <div class="col-sm-3 col-lg-3 mx-auto">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Nilai CKP Tertinggi</h6>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    {{ $nilai_ckp_tertinggi }}                    
-                                     
-                </div>
-            </div>
-        </div>
+                <div class="col-sm-3 col-lg-3 mx-auto">
+                    <div class="card shadow mb-4">
+                        <a href="/kepalabps-perencanaankerja/penilaianskp">
+                            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                <h6 class="m-0 font-weight-bold text-primary">Nilai SKP Terendah</h6>
+                            </div>
+                            <!-- Card Body -->
+                            <div class="card-body">
+                                {{ $nilai_skp_terendah }}    
+                            </div>
 
-        <div class="col-sm-3 col-md-3 mx-auto">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Nilai CKP Terendah</h6>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    {{ $nilai_ckp_terendah }}                       
-                                     
+                        </a>
+                        <!-- Card Header - Dropdown -->
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
+
+            <div class="row">
+                <div class="col-sm-3 col-lg-3 mx-auto">
+                    <div class="card shadow mb-4">
+                        <a href="/kepalabps-monitoring/monitoringckp">
+                            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                <h6 class="m-0 font-weight-bold text-primary">Nilai CKP Tertinggi</h6>
+                            </div>
+                            <!-- Card Body -->
+                            <div class="card-body">
+                                {{ $nilai_ckp_tertinggi }}    
+                            </div>
+                        </a>
+                        <!-- Card Header - Dropdown -->
+                    </div>
+                </div>
+
+                <div class="col-sm-3 col-md-3 mx-auto">
+                    <div class="card shadow mb-4">
+                        <a href="/kepalabps-monitoring/monitoringckp">
+                            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                <h6 class="m-0 font-weight-bold text-primary">Nilai CKP Terendah</h6>
+                            </div>
+                            <!-- Card Body -->
+                            <div class="card-body">
+                                {{ $nilai_ckp_terendah }}
     
-        <div class="col-xl-8 col-lg-7 mx-auto">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Grafik Penilaian CKP</h6>
+                            </div>
+                        
+                        </a>
+                        <!-- Card Header - Dropdown -->
+                    </div>
                 </div>
-                <!-- Card Body -->
-                <div class="card-body">                    
-                    {!! $chartckp->container() !!}                    
+            </div>
+
+            <div class="col-xl-8 col-lg-7 mx-auto">
+                <div class="card shadow mb-4">
+                    <!-- Card Header - Dropdown -->
+                    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                        <h6 class="m-0 font-weight-bold text-primary">Grafik Penilaian CKP</h6>
+                    </div>
+                    <!-- Card Body -->
+                    <div class="card-body">
+                        {!! $chartckp->container() !!}
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-8 col-lg-7 mx-auto">
+                <div class="card shadow mb-4">
+                    <!-- Card Header - Dropdown -->
+                    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                        <h6 class="m-0 font-weight-bold text-primary">Grafik Penilaian SKP</h6>
+                    </div>
+                    <!-- Card Body -->
+                    <div class="card-body">
+                        {!! $chartskp->container() !!}
+                    </div>
                 </div>
             </div>
         </div>
+        <!-- /.container-fluid -->
+        <script src="{{ $chartckp->cdn() }}"></script>
 
-        <div class="col-xl-8 col-lg-7 mx-auto">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Grafik Penilaian SKP</h6>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    {!! $chartskp->container() !!}
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- /.container-fluid -->
-    <script src="{{ $chartckp->cdn() }}"></script>
+        {{ $chartckp->script() }}
 
-    {{ $chartckp->script() }}
+        <script src="{{ $chartskp->cdn() }}"></script>
 
-    <script src="{{ $chartskp->cdn() }}"></script>
-
-    {{ $chartskp->script() }}
-
-
-@endsection
+        {{ $chartskp->script() }}
+    @endsection
