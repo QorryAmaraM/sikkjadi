b730a92130e451fc5a8f6e6cb30e18a2fc9ba959
wisnusetianto <chrmgawain@gmail.com>
2024-02-20 15:56:58 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ use App\Charts\grafikpenilaianckp;
 use App\Charts\grafikpenilaianskp;
 use Illuminate\Http\Request;
 use App\Models\user;
+use App\Models\penilaian_ckpr;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
@@ -22,29 +23,39 @@ class DashboardController extends Controller
 
         $nilai_skp_tertinggi = User::max('nilai_skp');
 
-        // dd($user);
-        
+        $result = penilaian_ckpr::join('ckprs', 'ckpr_id', '=', 'ckprs.id')
+            ->join('ckpts', 'ckpt_id', '=', 'ckpts.id')
+            ->join('users', 'user_id', '=', 'users.id')
+            ->join('entri_angka_kredits', 'angka_kredit_id', '=', 'entri_angka_kredits.id')
+            ->join('list_uraian_kegiatans', 'uraian_kegiatan_id', '=', 'list_uraian_kegiatans.id')
+            ->leftjoin('monitoring_ckps', 'monitoring_ckps.penilaian_ckpr_id', '=', 'penilaian_ckprs.id')
+            ->select('role_id', 'ckp_akhir')
+            ->get();
+
+        $nilai_ckp_terendah = $result->min('ckp_akhir');
+        $nilai_ckp_tertinggi = $result->max('ckp_akhir');
+
         switch ($userid) {
             case '1':
-                return view('pages.admin.dashboard.index', compact(['user', 'chartckp','chartskp']));
+                return view('pages.admin.dashboard.index', compact(['user', 'chartckp', 'chartskp']));
                 break;
             case '2':
-                return view('pages.users.kepalaBPS.dashboard.index', compact(['user', 'chartckp','chartskp', 'nilai_skp_tertinggi', 'nilai_skp_terendah']));
+                return view('pages.users.kepalaBPS.dashboard.index', compact(['user', 'chartckp', 'chartskp', 'nilai_skp_tertinggi', 'nilai_skp_terendah', 'nilai_ckp_tertinggi', 'nilai_ckp_terendah']));
                 break;
             case '3':
-                return view('pages.users.kepalaBU.dashboard.index', compact(['user', 'chartckp','chartskp']));
+                return view('pages.users.kepalaBU.dashboard.index', compact(['user', 'chartckp', 'chartskp']));
                 break;
             case '4':
-                return view('pages.users.KF.dashboard.index', compact(['user', 'chartckp','chartskp']));
+                return view('pages.users.KF.dashboard.index', compact(['user', 'chartckp', 'chartskp']));
                 break;
             case '5':
-                return view('pages.users.staf.dashboard.index', compact(['user', 'chartckp','chartskp']));
+                return view('pages.users.staf.dashboard.index', compact(['user', 'chartckp', 'chartskp']));
                 break;
         }
     }
 
     public function profil(Request $request)
-    {   
+    {
         $user = Auth::user();
         $userid = Auth::user()->role_id;
         // dd($user);
