a186ded4bf8bbe965294df95af0259dfe839508a
wisnusetianto <chrmgawain@gmail.com>
2024-02-20 09:40:51 +0700

    update

--- resources/views/pages/admin/monitoringuser/create.blade.php
+++ resources/views/pages/admin/monitoringuser/create.blade.php
@@ -49,13 +49,13 @@
                                         <div class="col-12 col-sm-6">
                                             <div class="form-group">
                                                 <label for="account-e-mail">NIP</label>
-                                                <input type="nip" class="form-control" name="nip" id="nip" required/>
+                                                <input type="nip" class="form-control" name="nip" id="nip" required />
                                             </div>
                                         </div>
                                         <div class="col-12 col-sm-6">
                                             <div class="form-group">
                                                 <label for="account-e-mail">Email</label>
-                                                <input type="email" class="form-control" name="email" id="email" required/>
+                                                <input type="email" class="form-control" name="email" id="email" required />
                                             </div>
                                         </div>
                                         <div class="col-12 col-sm-6">
@@ -67,13 +67,55 @@
                                         <div class="col-12 col-sm-6">
                                             <div class="form-group">
                                                 <label for="account-e-mail">Golongan</label>
-                                                <input type="golongan" class="form-control" name="golongan" id="golongan" required />
+                                                <select class="form-control col-sm-12" data-width="75%" data-live-search="true" id="golongan" name="golongan" required>
+                                                    <option value="">Pilih Fungsional</option>
+                                                    <option value="Pembina Utama (IV/e)">Pembina Utama (IV/e)</option>
+                                                    <option value="Pembina UtamaMadya (IV/d)">Pembina UtamaMadya (IV/d)</option>
+                                                    <option value="Pembina Utama Muda (IV/c)">Pembina Utama Muda (IV/c)</option> 
+                                                    <option value="Pembina Tingkat I (IV/b)">Pembina Tingkat I (IV/b)</option>
+                                                    <option value="Pembina (IV/a)">Pembina (IV/a)</option>
+                                                    <option value="Penata Tingkat I (III/d)">Penata Tingkat I (III/d)</option>
+                                                    <option value="Penata (III/c)">Penata (III/c)</option>
+                                                    <option value="Penata Muda Tingkat I (III/b)">Penata Muda Tingkat I (III/b)</option>
+                                                    <option value="Penata Muda (III/a)">Penata Muda (III/a)</option>
+                                                    <option value="Pengatur Tingkat I (II/d)">Pengatur Tingkat I (II/d)</option>
+                                                    <option value="Pengatur (II/c)">Pengatur (II/c)</option>
+                                                    <option value="Pengatur Muda Tingkat I (II/b)">Pengatur Muda Tingkat I (II/b)</option>
+                                                    <option value="Pengatur Muda (II/a)">Pengatur Muda (II/a)</option>
+                                                    <option value="Juru Tingkat I (I/d)">Juru Tingkat I (I/d)</option>
+                                                    <option value="Juru (I/c)">Juru (I/c)</option>
+                                                    <option value="Juru Muda Tingkat I (I/b)">Juru Muda Tingkat I (I/b)</option>
+                                                    <option value="Juru Muda (I/a)">Juru Muda (I/a)</option>
+                                                </select>
                                             </div>
                                         </div>
                                         <div class="col-12 col-sm-6">
                                             <div class="form-group">
                                                 <label for="account-e-mail">Fungsional</label>
-                                                <input type="fungsional" class="form-control" name="fungsional" id="fungsional" required/>
+                                                <select class="form-control col-sm-12" data-width="75%" data-live-search="true" id="fungsional" name="fungsional" required>
+                                                    <option value="">Pilih Fungsional</option>
+                                                    <option value="KEPALA KANTOR">KEPALA KANTOR </option>
+                                                    <option value="Fungsional Pranata Komputer Muda">Fungsional Pranata Komputer Muda</option>
+                                                    <option value="Statistisi Ahli Muda">Statistisi Ahli Muda</option>
+                                                    <option value="Statistisi Ahli Muda">Statistisi Ahli Muda</option>
+                                                    <option value="Statistisi Ahli Muda">Statistisi Ahli Muda</option>
+                                                    <option value="Statistisi Ahli Muda">Statistisi Ahli Muda</option>
+                                                    <option value="Statistisi Ahli Muda">Statistisi Ahli Muda</option>
+                                                    <option value="Pranata Keuangan APBN Terampil">Pranata Keuangan APBN Terampil</option>
+                                                    <option value="Tugas Belajar">Tugas Belajar</option>
+                                                    <option value="Statistisi Pelaksana Lanjutan">Statistisi Pelaksana Lanjutan</option>
+                                                    <option value="BENDAHARA PENGELUARAN">BENDAHARA PENGELUARAN</option>
+                                                    <option value="Tugas Belajar">Tugas Belajar</option>
+                                                    <option value="Staf Subbag Umum">Staf Subbag Umum</option>
+                                                    <option value="Statistisi Penyeli">Statistisi Penyeli</option>
+                                                    <option value="Statistisi Ahli Pertama">Statistisi Ahli Pertama</option>
+                                                    <option value="Statistisi Ahli Pertama">Statistisi Ahli Pertama</option>
+                                                    <option value="Tugas Belajar">Tugas Belajar</option>
+                                                    <option value="Pranata komputer ahli pertama">Pranata komputer ahli pertama</option>
+                                                    <option value="Statistisi Ahli Muda">Statistisi Ahli Muda</option>
+                                                    <option value="Statistisi Ahli Pertama">Statistisi Ahli Pertama</option>
+                                                    <option value="CPNS">CPNS</option>
+                                                </select>
                                             </div>
                                         </div>
                                     </div>
@@ -103,42 +145,41 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
         function checkFormAndShowModal() {
-    var form = document.getElementById('myForm');
-    var allInputsFilled = true;
+            var form = document.getElementById('myForm');
+            var allInputsFilled = true;
 
-    // Loop untuk memeriksa setiap input dalam form
-    for (var i = 0; i < form.length; i++) {
-        if (form[i].type == "text" || form[i].type == "select-one") {
-            if (form[i].value == "") {
-                allInputsFilled = false;
-                break;
+            // Loop untuk memeriksa setiap input dalam form
+            for (var i = 0; i < form.length; i++) {
+                if (form[i].type == "text" || form[i].type == "select-one") {
+                    if (form[i].value == "") {
+                        allInputsFilled = false;
+                        break;
+                    }
+                }
+            }
+
+            // Jika semua input terisi, tampilkan modal
+            if (allInputsFilled) {
+                Swal.fire({
+                    position: "top-center",
+                    icon: "success",
+                    title: "Data berhasil ditambah!",
+                    showConfirmButton: false,
+                    timer: 10000
+                }).then((result) => {
+                    if (result.dismiss === Swal.DismissReason.timer) {
+                        $('#successModal').modal('show');
+                    }
+                });
+            } else {
+                // Tampilkan peringatan SweetAlert jika tidak semua data terisi
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Harap isi semua data sebelum melanjutkan.',
+                });
             }
         }
-    }
-
-    // Jika semua input terisi, tampilkan modal
-    if (allInputsFilled) {
-        Swal.fire({
-            position: "top-center",
-            icon: "success",
-            title: "Data berhasil ditambah!",
-            showConfirmButton: false,
-            timer: 10000
-        }).then((result) => {
-            if (result.dismiss === Swal.DismissReason.timer) {
-                $('#successModal').modal('show');
-            }
-        });
-    } else {
-        // Tampilkan peringatan SweetAlert jika tidak semua data terisi
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Harap isi semua data sebelum melanjutkan.',
-        });
-    }
-}
-
     </script>
 @endsection
 <!-- /.container-fluid -->
