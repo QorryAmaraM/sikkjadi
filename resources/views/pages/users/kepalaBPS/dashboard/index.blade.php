da533cd2c961e75568e1c5160a4f6f016e1ce6d6
QorryAmaraM <qorryamaram1@gmail.com>
2024-02-20 16:26:24 +0700

    puash

--- resources/views/pages/users/kepalaBPS/dashboard/index.blade.php
+++ resources/views/pages/users/kepalaBPS/dashboard/index.blade.php
@@ -8,7 +8,7 @@
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             @foreach ($user as $users)
                 @if ($users->id == $userid)
-                    <h1 class="h3 mb-0 text-black-800">Selamat Datang di Sistem Informasi Kinerja Karyawan BPS {{ $users->nama }}</h1>
+                <h1 class="h3 mb-0 text-black-800">Selamat Datang di Sistem Informasi Kinerja Karyawan BPS {{ $users->nama }}</h1>
                 @endif
             @endforeach
             </h1>
@@ -16,61 +16,83 @@
 
         <div class="container-fluid">
     <div class="row">
-        <div class="col-sm-3 col-md-3 mx-auto">
+        <div class="col-xl-3 col-lg-7 mx-auto">
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Nilai SKP Tertinggi</h6>
-                </div>
                 <!-- Card Body -->
-                <div class="card-body"> 
-                    {{ $nilai_skp_tertinggi }}                      
-                                     
-                </div>
+                <a href="{{ route('admin_monitoringuser') }}" class="text-decoration-none">
+                    <div class="card-body cursor-pointer">                    
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-md-8 col-lg-6">
+                                <div class="text-center">
+                                    <h4 class="card-title">Nilai SKP Tertinggi</h4>
+                                    <div class="display-4 font-weight-bold">{{ $nilai_skp_tertinggi }} </div>
+                                </div>
+                            </div>
+                        </div>          
+                    </div>
+                </a>
             </div>
         </div>
 
-        <div class="col-sm-3 col-lg-3 mx-auto">
+
+        <div class="col-xl-3 col-lg-7 mx-auto">
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Nilai SKP Terendah</h6>
-                </div>
                 <!-- Card Body -->
-                <div class="card-body">
-                    {{ $nilai_skp_terendah }}                    
-                                     
-                </div>
+                <a href="{{ route('admin_monitoringuser') }}" class="text-decoration-none">
+                    <div class="card-body cursor-pointer">                    
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-md-8 col-lg-6">
+                                <div class="text-center">
+                                    <h4 class="card-title">Nilai SKP Terendah</h4>
+                                    <div class="display-4 font-weight-bold">{{ $nilai_skp_terendah }} </div>
+                                </div>
+                            </div>
+                        </div>          
+                    </div>
+                </a>
             </div>
         </div>
     </div>
 
     <div class="row">
-        <div class="col-sm-3 col-lg-3 mx-auto">
+        <div class="col-xl-3 col-lg-7 mx-auto">
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Nilai CKP Tertinggi</h6>
-                </div>
                 <!-- Card Body -->
-                <div class="card-body">
-                    {{ $nilai_ckp_tertinggi }}                    
-                                     
-                </div>
+                <a href="{{ route('admin_monitoringuser') }}" class="text-decoration-none">
+                    <div class="card-body cursor-pointer">                    
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-md-8 col-lg-6">
+                                <div class="text-center">
+                                    <h4 class="card-title">Nilai CKP Tertinggi</h4>
+                                    <div class="display-4 font-weight-bold">{{ $nilai_ckp_tertinggi }} </div>
+                                </div>
+                            </div>
+                        </div>          
+                    </div>
+                </a>
             </div>
         </div>
 
-        <div class="col-sm-3 col-md-3 mx-auto">
+
+        <div class="col-xl-3 col-lg-7 mx-auto">
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Nilai CKP Terendah</h6>
-                </div>
                 <!-- Card Body -->
-                <div class="card-body">
-                    {{ $nilai_ckp_terendah }}                       
-                                     
-                </div>
+                <a href="{{ route('admin_monitoringuser') }}" class="text-decoration-none">
+                    <div class="card-body cursor-pointer">                    
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-md-8 col-lg-6">
+                                <div class="text-center">
+                                    <h4 class="card-title">Nilai CKP Terendah</h4>
+                                    <div class="display-4 font-weight-bold">{{ $nilai_ckp_terendah }} </div>
+                                </div>
+                            </div>
+                        </div>          
+                    </div>
+                </a>
             </div>
         </div>
     </div>
@@ -88,25 +110,27 @@
             </div>
         </div>
 
-            <div class="col-xl-8 col-lg-7 mx-auto">
-                <div class="card shadow mb-4">
-                    <!-- Card Header - Dropdown -->
-                    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">Grafik Penilaian SKP</h6>
-                    </div>
-                    <!-- Card Body -->
-                    <div class="card-body">
-                        {!! $chartskp->container() !!}
-                    </div>
+        <div class="col-xl-8 col-lg-7 mx-auto">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Grafik Penilaian SKP</h6>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    {!! $chartskp->container() !!}
                 </div>
             </div>
         </div>
-        <!-- /.container-fluid -->
-        <script src="{{ $chartckp->cdn() }}"></script>
+    </div>
+    <!-- /.container-fluid -->
+    <script src="{{ $chartckp->cdn() }}"></script>
 
-        {{ $chartckp->script() }}
+    {{ $chartckp->script() }}
 
-        <script src="{{ $chartskp->cdn() }}"></script>
+    <script src="{{ $chartskp->cdn() }}"></script>
 
-        {{ $chartskp->script() }}
-    @endsection
+    {{ $chartskp->script() }}
+
+
+@endsection
