d2a314cfa6218574f8358cedc935af606889ab0b
wisnusetianto <chrmgawain@gmail.com>
2024-02-20 08:13:41 +0700

    update

--- resources/views/pages/users/kepalaBU/monitoringpre/create.blade.php
+++ resources/views/pages/users/kepalaBU/monitoringpre/create.blade.php
@@ -18,32 +18,27 @@
                 <div class="col-sm-12">
                     <div class="form-group d-flex align-items-center">
                         <label for="nama" class="col-sm-1 pl-0 col-form-label">Nama</label>
-                        @foreach ($user as $users)
-                            @if ($users->id == $userid)
-                                <input type="nama" class="form-control col-sm-11" id="nama"
-                                    placeholder="Lorem Ipsum" name="nama" value="{{ $users->nama }}" disabled>
-                                <input type="hidden" name="user_id" value="{{ $userid }}">
-                            @endif
-                        @endforeach
+                        <select class="form-control col-sm-11" data-width="75%" data-live-search="true" id="user_id" name="user_id">
+                            <option value="">Pilih Pegawai</option>
+                            @foreach ($user as $pegawai)
+                                <option value="{{ $pegawai->id }}">{{ $pegawai->nama }}</option>
+                            @endforeach
                     </div>
                     <div class="form-group d-flex align-items-center">
                         <label for="nip" class="col-sm-1 pl-0 col-form-label">NIP</label>
                         @foreach ($user as $users)
                             @if ($users->id == $userid)
-                                <input type="nama" class="form-control col-sm-11" id="nama"
-                                    placeholder="Lorem Ipsum" name="nip" value="{{ $users->nip }}" disabled>
+                                <input type="nama" class="form-control col-sm-11" id="nama" placeholder="Lorem Ipsum" name="nip" value="{{ $users->nip }}" disabled>
                             @endif
                         @endforeach
                     </div>
                     <div class="form-group d-flex align-items-center">
                         <label for="tahun" class="col-sm-1 pl-0 col-form-label">Tahun</label>
-                        <input type="tahun" class="form-control col-sm-11" id="tahun"
-                            placeholder="YYYY" name="tahun" required>
+                        <input type="tahun" class="form-control col-sm-11" id="tahun" placeholder="YYYY" name="tahun" required>
                     </div>
                     <div class="form-group d-flex align-items-center">
                         <label for="bulan" class="col-sm-1 pl-0 col-form-label">Bulan</label>
-                        <input type="bulan" class="form-control col-sm-11" id="bulan" placeholder="Bulan"
-                            name="bulan" required>
+                        <input type="bulan" class="form-control col-sm-11" id="bulan" placeholder="Bulan" name="bulan" required>
                     </div>
                 </div>
             </div>
@@ -52,83 +47,67 @@
                 <div class="col-sm-12">
                     <div class="form-group">
                         <label for="cp">CP</label>
-                        <input type="cp" class="form-control" id="cp" placeholder="Lorem Ipsum Dolor Sit Amet"
-                            name="cp">
+                        <input type="cp" class="form-control" id="cp" placeholder="Lorem Ipsum Dolor Sit Amet" name="cp">
                     </div>
                     <div class="form-group">
                         <label for="ct">CT</label>
-                        <input type="ct" class="form-control" id="ct" placeholder="Lorem Ipsum Dolor Sit Amet"
-                            name="ct">
+                        <input type="ct" class="form-control" id="ct" placeholder="Lorem Ipsum Dolor Sit Amet" name="ct">
                     </div>
                     <div class="form-group">
                         <label for="cb">CB</label>
-                        <input type="cb" class="form-control" id="cb" placeholder="Lorem Ipsum Dolor Sit Amet"
-                            name="cb">
+                        <input type="cb" class="form-control" id="cb" placeholder="Lorem Ipsum Dolor Sit Amet" name="cb">
                     </div>
                     <div class="form-group">
                         <label for="cs">CS</label>
-                        <input type="cs" class="form-control" id="cs" placeholder="Lorem Ipsum Dolor Sit Amet"
-                            name="cs">
+                        <input type="cs" class="form-control" id="cs" placeholder="Lorem Ipsum Dolor Sit Amet" name="cs">
                     </div>
                     <div class="form-group">
                         <label for="cm">CM</label>
-                        <input type="cm" class="form-control" id="cm" placeholder="Lorem Ipsum Dolor Sit Amet"
-                            name="cm">
+                        <input type="cm" class="form-control" id="cm" placeholder="Lorem Ipsum Dolor Sit Amet" name="cm">
                     </div>
                     <div class="form-group">
                         <label for="cltn">CLTN</label>
-                        <input type="cltn" class="form-control" id="cltn" placeholder="Lorem Ipsum Dolor Sit Amet"
-                            name="cltn">
+                        <input type="cltn" class="form-control" id="cltn" placeholder="Lorem Ipsum Dolor Sit Amet" name="cltn">
                     </div>
                     <div class="form-group">
                         <label for="s">S</label>
-                        <input type="s" class="form-control" id="s"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="s">
+                        <input type="s" class="form-control" id="s" placeholder="Lorem Ipsum Dolor Sit Amet" name="s">
                     </div>
                     <div class="form-group">
                         <label for="psw1">PSW1</label>
-                        <input type="psw1" class="form-control" id="psw1"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="psw1">
+                        <input type="psw1" class="form-control" id="psw1" placeholder="Lorem Ipsum Dolor Sit Amet" name="psw1">
                     </div>
                     <div class="form-group">
                         <label for="psw2">PSW2</label>
-                        <input type="psw2" class="form-control" id="psw2"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="psw2">
+                        <input type="psw2" class="form-control" id="psw2" placeholder="Lorem Ipsum Dolor Sit Amet" name="psw2">
                     </div>
                     <div class="form-group">
                         <label for="psw3">PSW3</label>
-                        <input type="psw3" class="form-control" id="psw3"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="psw3">
+                        <input type="psw3" class="form-control" id="psw3" placeholder="Lorem Ipsum Dolor Sit Amet" name="psw3">
                     </div>
                     <div class="form-group">
                         <label for="psw4">PSW4</label>
-                        <input type="psw4" class="form-control" id="psw4"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="psw4">
+                        <input type="psw4" class="form-control" id="psw4" placeholder="Lorem Ipsum Dolor Sit Amet" name="psw4">
                     </div>
                     <div class="form-group">
                         <label for="tl1">TL1</label>
-                        <input type="tl1" class="form-control" id="tl1"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="tl1">
+                        <input type="tl1" class="form-control" id="tl1" placeholder="Lorem Ipsum Dolor Sit Amet" name="tl1">
                     </div>
                     <div class="form-group">
                         <label for="tl2">TL2</label>
-                        <input type="tl2" class="form-control" id="tl2"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="tl2">
+                        <input type="tl2" class="form-control" id="tl2" placeholder="Lorem Ipsum Dolor Sit Amet" name="tl2">
                     </div>
                     <div class="form-group">
                         <label for="tl3">TL3</label>
-                        <input type="tl3" class="form-control" id="tl3"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="tl3">
+                        <input type="tl3" class="form-control" id="tl3" placeholder="Lorem Ipsum Dolor Sit Amet" name="tl3">
                     </div>
                     <div class="form-group">
                         <label for="tl4">TL4</label>
-                        <input type="tl4" class="form-control" id="tl4"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="tl4">
+                        <input type="tl4" class="form-control" id="tl4" placeholder="Lorem Ipsum Dolor Sit Amet" name="tl4">
                     </div>
                     <div class="form-group">
                         <label for="jhk">JHK</label>
-                        <input type="jhk" class="form-control" id="jhk"
-                            placeholder="Lorem Ipsum Dolor Sit Amet" name="jhk">
+                        <input type="jhk" class="form-control" id="jhk" placeholder="Lorem Ipsum Dolor Sit Amet" name="jhk">
                     </div>
                 </div>
             </div>
@@ -136,42 +115,42 @@
             <div class="row">
                 <div class="col-sm-12 mt-3 text-right">
 
-                    <button type="submit" name="submit" value="Save" class="btn save-button" onclick="checkFormAndShowModal()" >Simpan</button>
+                    <button type="submit" name="submit" value="Save" class="btn save-button" onclick="checkFormAndShowModal()">Simpan</button>
                 </div>
             </div>
 
-           
+
         </form>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
-       function checkFormAndShowModal() {
-    var tahun = document.getElementById('tahun').value.trim();
-    var bulan = document.getElementById('bulan').value.trim();
+        function checkFormAndShowModal() {
+            var tahun = document.getElementById('tahun').value.trim();
+            var bulan = document.getElementById('bulan').value.trim();
 
-    // Jika tahun atau bulan tidak diisi, tampilkan modal error
-    if (tahun === '' || bulan === '') {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Harap isi tahun dan bulan sebelum melanjutkan.',
-        });
-    } else {
-        // Jika tahun dan bulan terisi, tampilkan modal sukses
-        Swal.fire({
-            position: "top-center",
-            icon: "success",
-            title: "Data berhasil ditambah!",
-            showConfirmButton: false,
-            timer: 10000
-        }).then((result) => {
-            if (result.dismiss === Swal.DismissReason.timer) {
-                $('#successModal').modal('show');
+            // Jika tahun atau bulan tidak diisi, tampilkan modal error
+            if (tahun === '' || bulan === '') {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Harap isi tahun dan bulan sebelum melanjutkan.',
+                });
+            } else {
+                // Jika tahun dan bulan terisi, tampilkan modal sukses
+                Swal.fire({
+                    position: "top-center",
+                    icon: "success",
+                    title: "Data berhasil ditambah!",
+                    showConfirmButton: false,
+                    timer: 10000
+                }).then((result) => {
+                    if (result.dismiss === Swal.DismissReason.timer) {
+                        $('#successModal').modal('show');
+                    }
+                });
             }
-        });
-    }
-}
+        }
     </script>
     <!-- /.container-fluid -->
 @endsection
